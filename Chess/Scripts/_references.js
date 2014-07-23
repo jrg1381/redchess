359ae2ed720de382522b512a84bf37d09577b48e
James Gilmore <james.gilmore@red-gate.com>
2014-07-23 17:13:06 +0100

    Delete the file with the piece images in it and get rid of references to jcanvas.

--- Chess/Scripts/_references.js
+++ Chess/Scripts/_references.js
@@ -5,5 +5,4 @@
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 /// <reference path="knockout-2.2.0.debug.js" />
-/// <reference path="jcanvas.min.js" />
 
