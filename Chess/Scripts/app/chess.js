444211c29992b235d76d9681e253ac0b8734a04f
James Gilmore <james.gilmore@red-gate.com>
2014-08-02 16:10:58 +0100

    Add a spinny to the pgn parser.

--- Chess/Scripts/app/chess.js
+++ Chess/Scripts/app/chess.js
@@ -13,21 +13,6 @@ var pieceMapping = {
 
 var spinner = CreateSpinner();
 
-function CreateSpinner() {
-    return new Spinner({
-        top: "100%",
-        left: "100%",
-        lines: 12, // The number of lines to draw
-        length: 7, // The length of each line
-        width: 5, // The line thickness
-        radius: 10, // The radius of the inner circle
-        color: '#B57271', // #rbg or #rrggbb
-        speed: 1, // Rounds per second
-        trail: 100, // Afterglow percentage
-        shadow: false // Whether to render a shadow
-    });
-}
-
 function ParentOfSpinny()
 {
     if (chessBoard.orientation() == "black") {
---
+++ Chess/Scripts/app/spinner.js
@@ -0,0 +1,14 @@
+﻿function CreateSpinner() {
+    return new Spinner({
+        top: "100%",
+        left: "100%",
+        lines: 12, // The number of lines to draw
+        length: 7, // The length of each line
+        width: 5, // The line thickness
+        radius: 10, // The radius of the inner circle
+        color: '#B57271', // #rbg or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 100, // Afterglow percentage
+        shadow: false // Whether to render a shadow
+    });
+}
