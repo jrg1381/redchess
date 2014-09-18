4d09a4bb56277f3b744346668b571175e26c738c
James Gilmore <james.gilmore@red-gate.com>
2014-09-18 22:03:52 +0100

    Add creation date column.

--- Database/Tables/dbo.BoardDtoes.sql
+++ Database/Tables/dbo.BoardDtoes.sql
@@ -3,11 +3,12 @@ CREATE TABLE [dbo].[BoardDtoes]
 [GameId] [int] NOT NULL IDENTITY(1, 1),
 [UserIdWhite] [int] NOT NULL,
 [UserIdBlack] [int] NOT NULL,
-[Status] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Status] [nvarchar] (max) NULL,
 [GameOver] [bit] NOT NULL,
-[Fen] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[MayClaimDraw] AS (case when [GameOver]=(1) then CONVERT([bit],(0),0) else [dbo].[MayClaimDraw]([GameId]) end)
-) ON [PRIMARY]
+[Fen] [nvarchar] (max) NULL,
+[MayClaimDraw] AS (case when [GameOver]=(1) then CONVERT([bit],(0),(0)) else [dbo].[MayClaimDraw]([GameId]) end),
+[CreationDate] [datetime] NOT NULL DEFAULT (getutcdate())
+)
 GO
 ALTER TABLE [dbo].[BoardDtoes] ADD CONSTRAINT [PK_dbo.BoardDtoes] PRIMARY KEY CLUSTERED  ([GameId])
 GO
