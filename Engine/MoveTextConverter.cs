924f89c152c0ecd519134847677bc9ea2daad3ba
James Gilmore <james.gilmore@red-gate.com>
2015-02-25 15:13:05 +0000

    Class for converting moves into text

---
+++ Engine/MoveTextConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using Redchess.Engine.Interfaces;
+using RedChess.ChessCommon;
+using RedChess.ChessCommon.Enumerations;
+
+namespace Redchess.Engine
+{
+    internal class MoveTextConverter
+    {
+        private readonly IBoardExtended m_board;
+
+        public MoveTextConverter(IBoardExtended game)
+        {
+            m_board = game;
+        }
+
+        public string MoveAsText(IPiece piece, Location newLocation)
+        {
+            if (piece.Type.IsOfType(PieceType.Pawn))
+            {
+                return PawnMove(piece, newLocation);
+            }
+
+            return "";
+        }
+
+        private string PawnMove(IPiece piece, Location newLocation)
+        {
+            return newLocation.ToString().ToLower();
+        }
+    }
+}
---
+++ EngineTests/MoveConverterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Redchess.Engine;
+using RedChess.ChessCommon.Enumerations;
+
+namespace Redchess.EngineTests
+{
+    [TestFixture]
+    class MoveConverterTests : AbstractChessTest
+    {
+        [Test]
+        public void FirstMoveOfPawn()
+        {
+            var converter = new MoveTextConverter(m_normalBoard);
+            string move = converter.MoveAsText(m_normalBoard.GetContents(Location.E2), Location.E4);
+            Assert.AreEqual("e4",move, "Pawn move was not correctly converted to text");
+        }
+    }
+}
