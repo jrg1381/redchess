848a3fdaeaf3c7db304917293ad5b0a926ed6d31
James Gilmore <james.gilmore@red-gate.com>
2015-03-02 13:55:44 +0000

    Add column for persisting last move

--- Database/Tables/dbo.Boards.sql
+++ Database/Tables/dbo.Boards.sql
@@ -8,7 +8,8 @@ CREATE TABLE [dbo].[Boards]
 [Fen] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [CreationDate] [datetime] NOT NULL CONSTRAINT [DF__RG_Recove__Creat__73BA3083] DEFAULT (getutcdate()),
 [CompletionDate] [datetime] NOT NULL CONSTRAINT [DF_BoardDtoes_CompletionDate] DEFAULT (getutcdate()),
-[MayClaimDraw] AS (case when [GameOver]=(1) then CONVERT([bit],(0),(0)) else [dbo].[MayClaimDraw]([GameId]) end)
+[MayClaimDraw] AS (case when [GameOver]=(1) then CONVERT([bit],(0),(0)) else [dbo].[MayClaimDraw]([GameId]) end),
+[LastMove] [nvarchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
 ALTER TABLE [dbo].[Boards] ADD CONSTRAINT [PK_dbo.BoardDtoes] PRIMARY KEY CLUSTERED  ([GameId])
