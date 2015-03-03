8d5c35388782f83246f479637c7d35c3bca37da1
James Gilmore <james.gilmore@red-gate.com>
2015-03-03 11:38:01 +0000

    Add index on FK

--- Database/Tables/dbo.HistoryEntries.sql
+++ Database/Tables/dbo.HistoryEntries.sql
@@ -3,12 +3,13 @@ CREATE TABLE [dbo].[HistoryEntries]
 [HistoryId] [int] NOT NULL IDENTITY(1, 1),
 [GameId] [int] NOT NULL,
 [MoveNumber] [int] NOT NULL,
-[Fen] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Fen] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[Move] [nvarchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 )
+CREATE NONCLUSTERED INDEX [FK_GameId] ON [dbo].[HistoryEntries] ([GameId])
+
+ALTER TABLE [dbo].[HistoryEntries] ADD 
+CONSTRAINT [PK_dbo.HistoryEntries] PRIMARY KEY CLUSTERED  ([HistoryId])
 ALTER TABLE [dbo].[HistoryEntries] ADD
 CONSTRAINT [FK_Cascade] FOREIGN KEY ([GameId]) REFERENCES [dbo].[Boards] ([GameId]) ON DELETE CASCADE
-GO
-ALTER TABLE [dbo].[HistoryEntries] ADD CONSTRAINT [PK_dbo.HistoryEntries] PRIMARY KEY CLUSTERED  ([HistoryId])
-GO
-CREATE NONCLUSTERED INDEX [FK_gameId] ON [dbo].[HistoryEntries] ([GameId])
 GO
