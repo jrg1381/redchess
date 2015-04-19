611b2926510b9642ee8af36d1574ebcad4d98734
James Gilmore <james.gilmore@red-gate.com>
2015-04-19 11:34:55 +0100

    Some login form changes, and case-sensitivity again
    Make the setter for UserProfile lowercase the values.

--- WebEngine/Models/UserProfile.cs
+++ WebEngine/Models/UserProfile.cs
@@ -1,14 +1,23 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
 
 namespace RedChess.WebEngine.Models
 {
     [Table("UserProfile")]
     public class UserProfile
     {
+        private string m_underlyingUsername;
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
-        public string UserName { get; set; }
+
+        public string UserName
+        {
+            get { return m_underlyingUsername; }
+            set { m_underlyingUsername = value.ToLower(); }
+        }
     }
 }
