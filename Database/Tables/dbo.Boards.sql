e4346b12109d51dd3327591b31c862cab7860903
James Gilmore <james.gilmore@red-gate.com>
2015-04-27 13:21:47 +0100

    No reason to allow these fields to be null
    Fen is always defined for a board, and a username cannot be null.

--- Database/Tables/dbo.Boards.sql
+++ Database/Tables/dbo.Boards.sql
@@ -3,13 +3,13 @@ CREATE TABLE [dbo].[Boards]
 [GameId] [int] NOT NULL IDENTITY(1, 1),
 [UserIdWhite] [int] NOT NULL,
 [UserIdBlack] [int] NOT NULL,
-[Status] [nvarchar] (max) NULL,
+[Status] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [GameOver] [bit] NOT NULL,
-[Fen] [nvarchar] (max) NULL,
+[Fen] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [CreationDate] [datetime] NOT NULL CONSTRAINT [DF__RG_Recove__Creat__73BA3083] DEFAULT (getutcdate()),
 [CompletionDate] [datetime] NOT NULL CONSTRAINT [DF_BoardDtoes_CompletionDate] DEFAULT (getutcdate()),
-[LastMove] [nvarchar] (10) NULL,
 [MayClaimDraw] AS (case when [GameOver]=(1) then CONVERT([bit],(0),(0)) else [dbo].[MayClaimDraw]([GameId]) end),
+[LastMove] [nvarchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [UserIdWinner] [int] NULL
 )
 ALTER TABLE [dbo].[Boards] ADD
--- Database/Tables/dbo.UserProfile.sql
+++ Database/Tables/dbo.UserProfile.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[UserProfile]
 (
 [UserId] [int] NOT NULL IDENTITY(1, 1),
-[UserName] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[UserName] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 )
 GO
 ALTER TABLE [dbo].[UserProfile] ADD CONSTRAINT [PK_dbo.UserProfile] PRIMARY KEY CLUSTERED  ([UserId])
