e7ec6a52dfdbc1e90daa7e17eb4cf73498c05dd8
James Gilmore <james.gilmore@red-gate.com>
2015-07-05 13:55:25 +0100

    Remove javascript globals
    The Chess class encapsulates the previously global objects

--- Chess/Scripts/app/chess.js
+++ Chess/Scripts/app/chess.js
@@ -1,132 +1,8 @@
-﻿var currentPlayerColor;
-var gameId;
-var currentTurn;
-var boardLocked = false;
-var chessBoard;
-var isTimedGame = false;
+﻿Chess.prototype.processServerResponse = function (data) {
+    this.updateUi(data.fen);
 
-/* Unicode chess piece characters */
-var pieceMapping = {
-    "K": "\u2654", "Q": "\u2655", "N": "\u2658", "R": "\u2656", "P": "\u2659", "B": "\u2657",
-    "k": "\u265a", "q": "\u265b", "n": "\u265e", "r": "\u265c", "p": "\u265f", "b": "\u265d"
-};
-
-var spinner = CreateSpinner();
-
-function ParentOfSpinny()
-{
-    if (chessBoard.orientation() == "black") {
-        return $('#board').find(".square-e4");
-    }
-    else if (chessBoard.orientation() == "white") {
-        return $('#board').find(".square-d5");
-    }
-}
-
-function DoMove(source, target, piece) {
-    if ((target[1] == '8' && piece == 'wP') || (target[1] == '1' && piece == 'bP')) {
-        $("input#Start").val(source.toUpperCase());
-        $("input#End").val(target.toUpperCase());
-        $("#submitmove form").show();
-        $("#Promote").val("Queen");
-        return '';
-    }
-
-    PostMove(source.toUpperCase(), target.toUpperCase(), $("#Promote option:selected").text());
-    $("#submitmove form").hide();
-    $("#Promote").val([]);
-}
-
-function onDragStart(source, piece, position, orientation) {
-    if ((orientation === 'white' && piece.search(/^w/) === -1) ||
-        (orientation === 'black' && piece.search(/^b/) === -1) ||
-        !$("form").is(":hidden") || piece.search(currentPlayerColor) == -1 || currentTurn != currentPlayerColor || boardLocked) {
-        return false;
-    }
-}
-
-function PostMove(start, end, promote) {
-    if(start == end)
-        return;
-
-    spinner.spin(ParentOfSpinny()[0]);
-
-    if (isTimedGame) {
-        myClock.pauseClock();
-    }
-
-    $.post("/Board/PlayMove", {
-        "id": gameId,
-        "Start": start, 
-        "End": end, 
-        "Promote": promote, 
-        "__RequestVerificationToken": $('[name=__RequestVerificationToken]').val() }
-    ).done(ProcessServerResponse);
-}
-
-function Resign() {
-    if(!confirm("Are you sure you want to resign?")) {
-        return;
-    }
-
-    $.post("/Board/Resign", {
-        "id": gameId,
-        "__RequestVerificationToken": $('[name=__RequestVerificationToken]').val()
-    }).done(ProcessServerResponse);
-}
-
-function ClaimDraw() {
-    if(!confirm("Are you sure you want to claim a draw?")) {
-        return;
-    }
-
-    $.post("/Board/ClaimDraw", {
-        "id": gameId,
-        "__RequestVerificationToken": $('[name=__RequestVerificationToken]').val()
-    }).done(ProcessServerResponse);
-}
-
-function TellServerGameIsTimedOut(message, timedOutColor) {
-    $.post("/Board/TimedOut", {
-        "id": gameId,
-        "message": message,
-        "timedoutcolor" : timedOutColor,
-        "__RequestVerificationToken": $('[name=__RequestVerificationToken]').val()
-    }).done(ProcessServerResponse);
-}
-
-function EndGame() {
-    LockBoard();
-    if (isTimedGame) {
-        clearInterval(myClock.timerId);
-        $("div#readybutton").hide();
-    }
-    $("#turnindicator").text("GAME OVER");
-    $("#resignbutton").hide();
-    $("#drawbutton").hide();
-}
-
-function UpdateUi(fen) {
-    chessBoard.position(fen);
-
-    var splitFen = fen.split(" ");
-    var turn = splitFen[1];
-
-    if ($("#turnindicator").text() != "GAME OVER") {
-        currentTurn = turn;
-        $("#turnindicator").text((turn == "b" ? "Black" : "White") + " to play");
-    }
-
-    if (ParentOfSpinny()[0].hasChildNodes()) {
-        spinner.stop();
-    }
-}
-
-function ProcessServerResponse(data) {
-    UpdateUi(data.fen);
-
-    if (isTimedGame) {
-        myClock.startClockTicking();
+    if (this.isTimedGame) {
+        this.myClock.startClockTicking();
     }
 
     if (data.message) {
@@ -136,7 +12,7 @@ function ProcessServerResponse(data) {
         $("#messages").parent().css('visibility', 'hidden');
         $("#messages").html('&nbsp;');
     }
-   
+
     if (data.mayClaimDraw) {
         $("#drawbutton").show();
     }
@@ -146,22 +22,22 @@ function ProcessServerResponse(data) {
     }
 
     if (data.status == "RESIGN" || data.status == "TIME" || data.status == "DRAW") {
-        EndGame();
+        this.endGame();
         return;
     }
 
     if (data.status != "OK")
         return;
 
-    if (isTimedGame) {
-        myClock.SetLastSyncTime(myClock);
-        myClock.SyncClockWithServer(myClock);
+    if (this.isTimedGame) {
+        this.myClock.SetLastSyncTime(this.myClock);
+        this.myClock.SyncClockWithServer(this.myClock);
     }
 
     $("#lastmove").text(data.lastmove);
     $("#fen").text(data.fen);
-    UpdateTakenPieces(data.fen);
-    
+    this.updateTakenPieces(data.fen);
+
     var board = $('#board');
 
     board.find('.square-55d63').removeClass('highlight-white');
@@ -169,32 +45,199 @@ function ProcessServerResponse(data) {
     board.find('.square-' + data.moveto.toLowerCase()).addClass('highlight-white');
 
     //crappy
-    if(data.message.match(/(draw)|(mate)/i) != null) {
-        EndGame();
+    if (data.message.match(/(draw)|(mate)/i) != null) {
+        this.endGame();
         return;
     }
-}
+};
 
-function LockBoard() {
-    if (boardLocked)
-        return;
-    boardLocked = true;
-    ParentOfSpinny().prepend('<div class="lockIcon"></div>');
-}
+function Chess(gameId, currentPlayerColor, clock) {
+    this.gameId = gameId;
+    this.currentPlayerColor = currentPlayerColor;
+    this.boardLocked = false;
+    this.spinner = null;
+    this.isTimedGame = (clock != null);
+    this.myClock = clock;
 
-function UnlockBoard() {
-    if (!boardLocked)
+    /* Unicode chess piece characters */
+    this.pieceMapping = {
+        "K": "\u2654", "Q": "\u2655", "N": "\u2658", "R": "\u2656", "P": "\u2659", "B": "\u2657",
+        "k": "\u265a", "q": "\u265b", "n": "\u265e", "r": "\u265c", "p": "\u265f", "b": "\u265d"
+    };
+
+    this.onDragStart = function (source, piece, position, orientation) {
+        if ((orientation === 'white' && piece.search(/^w/) === -1) ||
+            (orientation === 'black' && piece.search(/^b/) === -1) ||
+            !$("form").is(":hidden") || piece.search(this.currentPlayerColor) == -1 || this.currentTurn != this.currentPlayerColor || this.boardLocked) {
+            return false;
+        }
+        return true;
+    };
+
+    var cfg = { pieceTheme: '/Images/{piece}.png', showNotation: false, draggable: true, onDrop: this.onDrop.bind(this), onDragStart : this.onDragStart };
+    this.chessBoard = new ChessBoard('board', cfg);
+
+    if (this.currentPlayerColor == 'b') {
+        this.chessBoard.flip();
+    }
+
+    //SignalR stuff
+    var updater = $.connection.updateServer;
+    // Define a client-side message which the server can call
+    updater.client.addMessage = function (message) {
+        this.processServerResponse(message);
+    }.bind(this);
+
+    // Define a callback for when the connection is established. We join the client group corresponding to our game ID.
+    $.connection.hub.start(function () {
+        $.connection.hub.proxies.updateserver.server.join(gameId);
+    });
+
+    // Hide the promotion UI and set its selection to nothing
+    $("#submitmove form").hide();
+    $("#Promote").val([]);
+
+    // Hide the claim the draw button
+    $("#drawbutton").hide();
+
+    $("#submitmove form").submit(function () {
+        this.postMove($("input#Start").val(), $("input#End").val(), $("#Promote option:selected").text());
+        $("#submitmove form").hide();
+        $("#Promote").val([]);
+        return false;
+    }.bind(this));
+};
+
+Chess.prototype.postMove = function (start, end, promote) {
+    var gameId = this.gameId;
+
+    if (start == end)
         return;
-    boardLocked = false;
+
+    this.getSpinner().spin(this.parentOfSpinny()[0]);
+
+    if (this.isTimedGame) {
+        this.myClock.pauseClock();
+    }
+
+    $.post("/Board/PlayMove", {
+        "id": gameId,
+        "Start": start,
+        "End": end,
+        "Promote": promote,
+        "__RequestVerificationToken": $('[name=__RequestVerificationToken]').val()
+    }
+    ).done(this.processServerResponse.bind(this));
+};
+
+Chess.prototype.onDrop = function (source, target, piece) {
+    if ((target[1] == '8' && piece == 'wP') || (target[1] == '1' && piece == 'bP')) {
+        $("input#Start").val(source.toUpperCase());
+        $("input#End").val(target.toUpperCase());
+        $("#submitmove form").show();
+        $("#Promote").val("Queen");
+        return '';
+    }
+
+    this.postMove(source.toUpperCase(), target.toUpperCase(), $("#Promote option:selected").text());
+    $("#submitmove form").hide();
+    $("#Promote").val([]);
+};
+
+Chess.prototype.getSpinner = function () {
+    if (this.spinner == null) {
+        this.spinner = CreateSpinner();
+    }
+    return this.spinner;
+};
+
+Chess.prototype.parentOfSpinny = function() {
+    if (this.chessBoard.orientation() == "black") {
+        return $('#board').find(".square-e4");
+    } else if (this.chessBoard.orientation() == "white") {
+        return $('#board').find(".square-d5");
+    }
+};
+
+Chess.prototype.resignGame = function() {
+    if (!confirm("Are you sure you want to resign?")) {
+        return;
+    }
+
+    $.post("/Board/Resign", {
+        "id": this.gameId,
+        "__RequestVerificationToken": $('[name=__RequestVerificationToken]').val()
+    }).done(this.processServerResponse.bind(this));
+};
+
+Chess.prototype.claimDraw = function() {
+    if (!confirm("Are you sure you want to claim a draw?")) {
+        return;
+    }
+
+    $.post("/Board/ClaimDraw", {
+        "id": this.gameId,
+        "__RequestVerificationToken": $('[name=__RequestVerificationToken]').val()
+    }).done(this.processServerResponse.bind(this));
+};
+
+Chess.prototype.tellServerGameIsTimedOut = function(message, timedOutColor) {
+    $.post("/Board/TimedOut", {
+        "id": this.gameId,
+        "message": message,
+        "timedoutcolor": timedOutColor,
+        "__RequestVerificationToken": $('[name=__RequestVerificationToken]').val()
+    }).done(this.processServerResponse.bind(this));
+};
+
+Chess.prototype.endGame = function() {
+    this.lockBoard();
+    if (this.isTimedGame) {
+        clearInterval(this.myClock.timerId);
+        $("div#readybutton").hide();
+    }
+    $("#turnindicator").text("GAME OVER");
+    $("#resignbutton").hide();
+    $("#drawbutton").hide();
+};
+
+Chess.prototype.updateUi = function(fen) {
+    this.chessBoard.position(fen);
+
+    var splitFen = fen.split(" ");
+    var turn = splitFen[1];
+
+    if ($("#turnindicator").text() != "GAME OVER") {
+        this.currentTurn = turn;
+        $("#turnindicator").text((turn == "b" ? "Black" : "White") + " to play");
+    }
+
+    if (this.parentOfSpinny()[0].hasChildNodes()) {
+        this.getSpinner().stop();
+    }
+};
+
+Chess.prototype.lockBoard = function() {
+    if (this.boardLocked)
+        return;
+    this.boardLocked = true;
+    this.parentOfSpinny().prepend('<div class="lockIcon"></div>');
+};
+
+Chess.prototype.unlockBoard = function() {
+    if (!this.boardLocked)
+        return;
+    this.boardLocked = false;
     $(".lockIcon").remove();
-}
-   
-function UpdateTakenPieces(fen) {
+};
+
+Chess.prototype.updateTakenPieces = function (fen) {
+    var pieceMapping = this.pieceMapping;
     var initialFen = fen.split(" ")[0];
     var blackArmy = "rrnnbbqpppppppp";
     var whiteArmy = "RRNNBBQPPPPPPPP";
-        
-    for(var i=0;i<initialFen.length;i++) {
+
+    for (var i = 0; i < initialFen.length; i++) {
         var c = initialFen[i];
         blackArmy = blackArmy.replace(c, "");
         whiteArmy = whiteArmy.replace(c, "");
@@ -219,47 +262,12 @@ function UpdateTakenPieces(fen) {
     var whitePieces = partialWhitePieces[0];
     var blackPieces = partialBlackPieces[0];
     if (partialWhitePieces.length > 1) {
-        whitePawnsTaken = pieceMapping['P'] + "<span style=\"font-size : medium\">" + partialWhitePieces[1] + "</span>";
+        whitePawnsTaken = this.pieceMapping['P'] + "<span style=\"font-size : medium\">" + partialWhitePieces[1] + "</span>";
     }
     if (partialBlackPieces.length > 1) {
-        blackPawnsTaken = pieceMapping['p'] + "<span style=\"font-size : medium\">" + partialBlackPieces[1] + "</span>";
+        blackPawnsTaken = this.pieceMapping['p'] + "<span style=\"font-size : medium\">" + partialBlackPieces[1] + "</span>";
     }
 
-    $("#blacktaken").html(blackPieces.split("").map(function (x) { return pieceMapping[x]; }).join("&#8203;") + blackPawnsTaken);
-    $("#whitetaken").html(whitePieces.split("").map(function (x) { return pieceMapping[x]; }).join("&#8203;") + whitePawnsTaken);
-}
-
-function DocumentReady() {
-    var cfg = { pieceTheme: '/Images/{piece}.png', showNotation: false, draggable: true, onDrop: DoMove, onDragStart: onDragStart };
-    chessBoard = new ChessBoard('board', cfg);
-
-    if (currentPlayerColor == 'b') {
-        chessBoard.flip();
-    }
-
-    //SignalR stuff
-    var updater = $.connection.updateServer;
-    // Define a client-side message which the server can call
-    updater.client.addMessage = function (message) {
-        ProcessServerResponse(message);
-    };
-
-    // Define a callback for when the connection is established. We join the client group corresponding to our game ID.
-    $.connection.hub.start(function () {
-        $.connection.hub.proxies.updateserver.server.join(gameId);
-    });
-
-    // Hide the promotion UI and set its selection to nothing
-    $("#submitmove form").hide();
-    $("#Promote").val([]);
-
-    // Hide the claim the draw button
-    $("#drawbutton").hide();
-
-    $("#submitmove form").submit(function () {
-        PostMove($("input#Start").val(), $("input#End").val(), $("#Promote option:selected").text());
-        $("#submitmove form").hide();
-        $("#Promote").val([]);
-        return false;
-    });
-}
+    $("#blacktaken").html(blackPieces.split("").map(function(x) { return pieceMapping[x]; }).join("&#8203;") + blackPawnsTaken);
+    $("#whitetaken").html(whitePieces.split("").map(function(x) { return pieceMapping[x]; }).join("&#8203;") + whitePawnsTaken);
+};
--- Chess/Scripts/app/clock.js
+++ Chess/Scripts/app/clock.js
@@ -1,18 +1,18 @@
-﻿function Clock(gameId, initialTimelimit, currentPlayerColor) {
+﻿function Clock(gameId, initialTimelimit) {
     this.gameId = gameId;
     this.lastServerBlackTimeRemaining = initialTimelimit;
     this.lastServerWhiteTimeRemaining = initialTimelimit;
     this.isTimerEnabled = false;
     this.timerId = -1;
-    this.currentPlayerColor = currentPlayerColor;
+    this.theChess = null;
 
     this.GetLastSyncTime = function() {
         return $.cookie("PageLastSeen" + this.gameId);
-    }
+    };
 
     this.SetLastSyncTime = function() {
         $.cookie("PageLastSeen" + this.gameId, (new Date()).getTime());
-    }
+    };
 
     this.InitializeClockTimeLimits = function() {
         $.post("/Clock/RefreshClock", { "id": gameId }).done(function(data) {
@@ -21,22 +21,22 @@
             $("#whitetime").text(new Date(this.lastServerWhiteTimeRemaining).toUTCString().substring(25, 17));
             $("#blacktime").text(new Date(this.lastServerBlackTimeRemaining).toUTCString().substring(25, 17));
         }.bind(this));
-    }
+    };
 
-    this.startClockTicking = function () {
+    this.startClockTicking = function() {
         clearInterval(this.timerId); // Don't run multiple clocks
-        this.timerId = setInterval(function () { this.LocalTimeCorrection(); }.bind(this), 1000);
+        this.timerId = setInterval(function() { this.LocalTimeCorrection(); }.bind(this), 1000);
         this.isTimerEnabled = true;
-    }
+    };
 
     this.pauseClock = function() {
         clearInterval(this.timerId);
         this.isTimerEnabled = false;
-    }
+    };
 
     this.SyncClockWithServer = function() {
         // if we haven't already done so...
-        UnlockBoard();
+        this.theChess.unlockBoard();
         // Now update the time
         $.post("/Clock/RefreshClock", { "id": gameId }).done(function(data) {
             this.lastServerBlackTimeRemaining = data.timeleftblack;
@@ -47,7 +47,7 @@
                 this.isTimerEnabled = true;
             }
         }.bind(this));
-    }
+    };
 
     // This updates the UI with the time elapsed for each player    
     this.LocalTimeCorrection = function() {
@@ -55,7 +55,7 @@
 
         var diff = (new Date()).getTime() - this.GetLastSyncTime();
 
-        if (currentTurn == "b") {
+        if (this.theChess.currentTurn == "b") {
             displayTimeWhite = new Date(this.lastServerWhiteTimeRemaining).toUTCString().substring(25, 17);
             displayTimeBlack = new Date(this.lastServerBlackTimeRemaining - diff).toUTCString().substring(25, 17);
             $("#whitetime").text(displayTimeWhite);
@@ -67,11 +67,11 @@
                 $("#messages").text("Black is out of time");
                 $("#blacktime").text("--:--");
                 $("#turnindicator").text("GAME OVER");
-                TellServerGameIsTimedOut("Black is out of time", "b");
+                this.theChess.tellServerGameIsTimedOut("Black is out of time", "b");
             }
         }
 
-        if (currentTurn == "w") {
+        if (this.theChess.currentTurn == "w") {
             displayTimeWhite = new Date(this.lastServerWhiteTimeRemaining - diff).toUTCString().substring(25, 17);
             displayTimeBlack = new Date(this.lastServerBlackTimeRemaining).toUTCString().substring(25, 17);
             $("#whitetime").text(displayTimeWhite);
@@ -83,32 +83,53 @@
                 $("#messages").text("White is out of time");
                 $("#whitetime").text("--:--");
                 $("#turnindicator").text("GAME OVER");
-                TellServerGameIsTimedOut("White is out of time", "w");
+                this.theChess.tellServerGameIsTimedOut("White is out of time", "w");
             }
         }
-    }
+    };
+
+    // Called when SignalR tells us that the other player is ready to play
+    this.ReadyToPlay = function(data) {
+        if (data.who == "w") {
+            $("td#whiteready").removeClass("notready");
+            $("td#whiteready").addClass("ready");
+            $("td#whiteready").text("READY");
+        } else if (data.who == "b") {
+            $("td#blackready").removeClass("notready");
+            $("td#blackready").addClass("ready");
+            $("td#blackready").text("READY");
+        }
+
+        if (data.status == "OK") {
+            if (null == this.GetLastSyncTime()) {
+                this.SetLastSyncTime();
+            }
+
+            this.SyncClockWithServer();
+        }
+    };
 
     this.ClockDocumentReady = function() {
         //SignalR stuff
         var updater = $.connection.updateServer;
         // Define a client-side message which the server can call
         updater.client.startClock = function(message) {
-            ReadyToPlay(message);
-        };
+            this.ReadyToPlay(message);
+        }.bind(this);
 
         this.InitializeClockTimeLimits();
 
-        if (this.currentPlayerColor == "") {
+        if (this.theChess.currentPlayerColor == "") {
             $("td#ready").hide();
             return;
         }
 
-        if (this.currentPlayerColor == "w" && $("td#whiteready").text() == "READY") {
+        if (this.theChess.currentPlayerColor == "w" && $("td#whiteready").text() == "READY") {
             $("td#ready").hide();
             return;
         }
 
-        if (this.currentPlayerColor == "b" && $("td#blackready").text() == "READY") {
+        if (this.theChess.currentPlayerColor == "b" && $("td#blackready").text() == "READY") {
             $("td#ready").hide();
             return;
         }
@@ -116,38 +137,16 @@
         $("div#readybutton").click(function() {
             $.post("/Clock/PlayerReady", { id: gameId }).done(function(data) {
                 $("td#ready").hide();
-                if (this.currentPlayerColor == "w") {
+                if (this.theChess.currentPlayerColor == "w") {
                     $("td#whiteready").removeClass("notready");
                     $("td#whiteready").addClass("ready");
                     $("td#whiteready").text("READY");
-                } else if (this.currentPlayerColor == "b") {
+                } else if (this.theChess.currentPlayerColor == "b") {
                     $("td#blackready").removeClass("notready");
                     $("td#blackready").addClass("ready");
                     $("td#blackready").text("READY");
                 }
             }.bind(this));
-        });
+        }.bind(this));
     };
-}
-
-// Called when SignalR tells us that the other player is ready to play
-function ReadyToPlay(data) {
-    if (data.who == "w") {
-        $("td#whiteready").removeClass("notready");
-        $("td#whiteready").addClass("ready");
-        $("td#whiteready").text("READY");
-    }
-    else if (data.who == "b") {
-        $("td#blackready").removeClass("notready");
-        $("td#blackready").addClass("ready");
-        $("td#blackready").text("READY");
-    }
-
-    if (data.status == "OK") {
-        if (null == this.myClock.GetLastSyncTime()) {
-            this.myClock.SetLastSyncTime();
-        }
-
-        this.myClock.SyncClockWithServer();
-    }
 }
