b003b99bed7d02f2fef92f2fb0c88a583f6101cb
James Gilmore <james.gilmore@red-gate.com>
2015-07-21 14:08:16 +0100

    Make java grammar generation always report success
    Although it's not ideal to have checked in the generated files, it will have to stay that way until deployment isn't done via github trigger.

--- PgnParser/BuildGrammar.bat
+++ PgnParser/BuildGrammar.bat
@@ -1,9 +1,12 @@
 
-IF DEFINED TEAMCITY_JRE (SET JAVA="%TEAMCITY_JRE%\bin\java.exe") ELSE (SET JAVA="C:\Program Files (x86)\Java\jre1.8.0_31\bin\java.exe")
+IF DEFINED TEAMCITY_JRE (SET JAVA="%TEAMCITY_JRE%\bin\java.exe") ELSE (SET JAVA="C:\Program Files (x86)\Java\jre1.8.0_40\bin\java.exe")
 SET PROJECT=%~1
 SET SOLUTION=%~2
 
-REM del /s/q "%PROJECT%Parser\*"
+echo %JAVA%
+
+rem del /s/q "%PROJECT%Parser\*"
 %JAVA% -jar "%SOLUTION%antlr\antlr-3.5.1-complete.jar" -make -report -o "%PROJECT%Parser" "%PROJECT%Pgn.g" 2>errors.txt
-type errors.txt
+type errors.txt | find /v "warning"
+EXIT /B 0
 
