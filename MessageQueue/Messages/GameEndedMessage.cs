0fe33262a29fe59a54ead54c87c4a02e76357ec3
James Gilmore <james.gilmore@red-gate.com>
2015-09-11 17:05:25 +0100

    Add the analysis worker and service projects

---
+++ MessageQueue/Messages/GameEndedMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RedChess.MessageQueue.Messages
+{
+    class GameEndedMessage
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int GameId { get; set; }
+        [JsonProperty(PropertyName = "pgn")]
+        public string Pgn { get; set; }
+    }
+}
