d7b473b3e313f0c7e66a1baf7d8fdd4b826bb644
James Gilmore <james.gilmore@red-gate.com>
2015-09-20 21:21:28 +0100

    Only let one thread download the engine at a time

--- AnalysisWorker/EngineDownloader.cs
+++ AnalysisWorker/EngineDownloader.cs
@@ -10,6 +10,7 @@ namespace Redchess.AnalysisWorker
 {
     static class EngineDownloader
     {
+        private static readonly object s_downloaderLock = new object();
         private static readonly byte[] s_expectedHash = {0x41,0xf3,0x8e,0xb5,0xa8,0xae,0x98,0x59,0x6a,0x63,0x9f,0x84,0x11,0x36,0x49,0x0b,0xf9,0xe4,0x4c,0xcc};
         private const string c_downloadUrl = "https://www.dropbox.com/s/e2jxxydxbnzqf4b/stockfish-6-64.exe?raw=1";
 
@@ -20,25 +21,32 @@ namespace Redchess.AnalysisWorker
             var targetDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string target = Path.Combine(targetDirectory, Path.GetFileName(targetFileName));
 
-            if (!File.Exists(target))
+            // Only one thread is allowed to download the engine at a time
+            lock (s_downloaderLock)
             {
-                Trace.WriteLine("Downloading engine");
-                using (var client = new WebClient())
+                if (!File.Exists(target))
                 {
-                    client.DownloadFile(c_downloadUrl, target);
-                }
-                Trace.WriteLine("Engine downloaded to " + target);
+                    Trace.WriteLine("Downloading engine");
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile(c_downloadUrl, target);
+                    }
+                    Trace.WriteLine("Engine downloaded to " + target);
 
-                byte[] engineHash;
-                using (var stream = new FileStream(target, FileMode.Open, FileAccess.Read))
-                {
-                    engineHash = SHA1.Create().ComputeHash(stream);
-                }
+                    byte[] engineHash;
+                    using (var stream = new FileStream(target, FileMode.Open, FileAccess.Read))
+                    {
+                        engineHash = SHA1.Create().ComputeHash(stream);
+                    }
 
-                if (!engineHash.SequenceEqual(s_expectedHash))
-                {
-                    File.Delete(target);
-                    throw new InvalidOperationException("Engine checksum invalid");
+                    if (!engineHash.SequenceEqual(s_expectedHash))
+                    {
+                        File.Delete(target);
+                        Trace.WriteLine("Engine checksum invalid");
+                        throw new InvalidOperationException("Engine checksum invalid");
+                    }
+
+                    Trace.WriteLine("Successfully downloaded the engine");
                 }
             }
 
