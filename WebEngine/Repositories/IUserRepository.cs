536289ecdd297cfa3c742e0447145c2013d7f79f
James Gilmore <james.gilmore@red-gate.com>
2015-09-24 16:12:09 +0100

    And the interface

---
+++ WebEngine/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RedChess.WebEngine.Models;
+
+namespace RedChess.WebEngine.Repositories
+{
+    interface IUserProfileRepository
+    {
+        IEnumerable<UserProfile> FindAll();
+        int UserId(string currentUser);
+    }
+}
