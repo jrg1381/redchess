803e6cd17d47214311fa210d9ee578cc652c9ac6
James Gilmore <james.gilmore@red-gate.com>
2015-10-19 17:45:00 +0100

    Begin work on the listener pattern

--- PgnProcessor/PgnParserImpl.cs
+++ PgnProcessor/PgnParserImpl.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Mime;
 using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 using Redchess.Pgn;
 using RedChess.ChessCommon.Interfaces;
 using RedChess.ChessCommon;
@@ -9,6 +11,41 @@ using RedChess.ChessCommon.Enumerations;
 
 namespace RedChess.PgnProcessor
 {
+    public class PgnListenerImpl : PgnBaseListener
+    {
+        private readonly Dictionary<string, string> m_tags;
+
+        public PgnListenerImpl(Dictionary<string, string> tags)
+        {
+            m_tags = tags;
+        }
+
+        public override void EnterBlackTag(PgnParser.BlackTagContext context)
+        {
+            m_tags["Black"] = context.bar.Text;
+        }
+
+        public override void EnterWhiteTag(PgnParser.WhiteTagContext context)
+        {
+            m_tags["White"] = context.bar.Text;
+        }
+
+        public override void EnterResultTag(PgnParser.ResultTagContext context)
+        {
+            m_tags["Result"] = context.bar.Text;
+        }
+
+        public override void EnterEventTag(PgnParser.EventTagContext context)
+        {
+            m_tags["Event"] = context.bar.Text;
+        }
+
+        public override void EnterOptionalTag(PgnParser.OptionalTagContext context)
+        {
+            m_tags[context.bar.Text] = context.foo.Text;
+        }
+    }
+
     public class PgnParserImpl : IParse
     {
         private Dictionary<string, string> m_tags;
@@ -25,24 +62,20 @@ namespace RedChess.PgnProcessor
                 BuildParseTree = true,
             };
             var tree = parser.parse();
-
-          //  Console.WriteLine(tree.ToStringTree(parser));
-
-            foreach (var kvp in parser.OptionalTags)
-            {
-                m_tags.Add(kvp.Key, kvp.Value);
-            }
-
-            m_tags["Black"] = parser.Black;
-            m_tags["White"] = parser.White;
-
-            Event = parser.Event;
-            Result = parser.Result;
+            var walker = new ParseTreeWalker();
+            var listener = new PgnListenerImpl(m_tags);
+            walker.Walk(listener, tree);
         }
 
+        public string Event
+        {
+            get { return m_tags["Event"]; }
+        }
 
-        public string Event { get; set; }
-        public string Result { get; set; }
+        public string Result
+        {
+            get { return m_tags["Result"]; }
+        }
 
         public IDictionary<string, string> Tags
         {
