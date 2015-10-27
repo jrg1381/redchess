7bd4ed740f706f9f5a6ed10d81dae745eb52f3c3
James Gilmore <james.gilmore@red-gate.com>
2015-10-27 21:18:23 +0000

    Variable rename

--- ChessCommon/ExtensionMethods.cs
+++ ChessCommon/ExtensionMethods.cs
@@ -1,17 +1,17 @@
-﻿using RedChess.ChessCommon.Enumerations;
+﻿using System.Diagnostics;
+using RedChess.ChessCommon.Enumerations;
 
 namespace RedChess.ChessCommon
 {
     public static class ExtensionMethods
     {
-        private static readonly PieceType Colors = PieceType.Black | PieceType.White;
-        private static readonly PieceType NotColors = ~Colors;
+        private const PieceType c_colors = PieceType.Black | PieceType.White;
 
         public static bool IsOfType(this PieceType pieceType, PieceType targetPieceType)
         {
             // Test is comparing with an 'abstract' piece, King, Queen, Pawn, etc
-            if ((targetPieceType & Colors) == PieceType.None)
-                return ((pieceType & targetPieceType) > Colors);
+            if ((targetPieceType & c_colors) == PieceType.None)
+                return ((pieceType & targetPieceType) > c_colors);
 
             // Test is for a specific color of piece
             return pieceType == targetPieceType;
