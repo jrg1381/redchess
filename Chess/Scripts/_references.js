b15ddd515f905ed688ca5f10e3d2df122054d98c
James Gilmore <james.gilmore@red-gate.com>
2015-11-07 22:28:50 +0000

    Update The World

--- Chess/Scripts/_references.js
+++ Chess/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-2.1.1.js" />
-/// <reference path="jquery-ui-1.10.4.js" />
+﻿/// <reference path="jquery-ui-1.11.4.js" />
+/// <reference path="jquery-2.1.4.js" />
 /// <reference path="jquery.signalR-2.1.0.min.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
