272bb737f223f6f7dc22c3e74edd03d472b0d37e
James Gilmore <james.gilmore@red-gate.com>
2015-11-18 21:22:15 +0000

    Some common overrides

--- ChessCommon/WorkItemResponse.cs
+++ ChessCommon/WorkItemResponse.cs
@@ -1,4 +1,5 @@
-﻿using RedChess.ChessCommon.Enumerations;
+﻿using System;
+using RedChess.ChessCommon.Enumerations;
 using RedChess.ChessCommon.Interfaces;
 
 namespace RedChess.ChessCommon
@@ -19,5 +20,38 @@ namespace RedChess.ChessCommon
         public string Analysis { get; set; }
         public int BoardEvaluation { get; set; }
         public EvaluationType BoardEvaluationType { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}:{1}:{2}", BoardEvaluation, BoardEvaluationType, Analysis);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var analysis = obj as BoardAnalysis;
+
+            if (analysis != null)
+            {
+                return Equals(analysis);
+            }
+
+            return false;
+        }
+
+        protected bool Equals(BoardAnalysis other)
+        {
+            return string.Equals(Analysis, other.Analysis) && BoardEvaluation == other.BoardEvaluation && BoardEvaluationType == other.BoardEvaluationType;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Analysis != null ? Analysis.GetHashCode() : 0);
+                hashCode = (hashCode*397) ^ BoardEvaluation;
+                hashCode = (hashCode*397) ^ (int) BoardEvaluationType;
+                return hashCode;
+            }
+        }
     }
 }
