cd91a2d366f727229ae01dbf24b3ad4a11732d5b
James Gilmore <james.gilmore@red-gate.com>
2015-11-18 21:23:24 +0000

    Take analysis simplifier out into a different class

---
+++ ControllerTests/AnalysisSimplifierTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using RedChess.ChessCommon;
+using RedChess.ChessCommon.Enumerations;
+using RedChess.WebEngine.Models;
+using RedChess.WebEngine.Repositories;
+using RedChess.WebEngine.Repositories.Interfaces;
+using Rhino.Mocks;
+
+namespace RedChess.ControllerTests
+{
+    [TestFixture]
+    public class AnalysisSimplifierTests
+    {
+        IHistoryRepository GetFakeHistoryRepo(HistoryEntry historyEntry)
+        {
+            var mock = MockRepository.GenerateMock<IHistoryRepository>();
+
+            mock.Expect(m => m.FindByGameIdAndMoveNumber(historyEntry.GameId, historyEntry.MoveNumber))
+                .Return(historyEntry);
+
+            return mock;
+        }
+
+        [Test]
+        public void NonMateInNUnchanged()
+        {
+            var historyEntry = new HistoryEntry
+            {
+                GameId = 10,
+                MoveNumber = 1,
+                Fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0",
+                Move = "Nc3"
+            };
+
+            var mock = GetFakeHistoryRepo(historyEntry);
+            var processor = new AnalysisSimplifier(mock);
+            var boardAnalysis = new BoardAnalysis
+            {
+                BoardEvaluation = 44,
+                Analysis = "doesn't matter",
+                BoardEvaluationType = EvaluationType.Centipawn
+            };
+
+            var newAnalysis = processor.ProcessBoardAnalysis(10, 1, boardAnalysis);
+            Assert.AreEqual(newAnalysis, boardAnalysis, "Expected analysis text to be unchanged for non-mate analysis");
+            mock.VerifyAllExpectations();
+        }
+
+        [Test]
+        public void MateInNModified()
+        {
+            var historyEntry = new HistoryEntry
+            {
+                GameId = 10,
+                MoveNumber = 1,
+                Fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0",
+                Move = "Nc3"
+            };
+
+            var mock = GetFakeHistoryRepo(historyEntry);
+            var processor = new AnalysisSimplifier(mock);
+            var boardAnalysis = new BoardAnalysis
+            {
+                BoardEvaluation = -11,
+                Analysis = "score mate -11 nodes 4884896 nps 1051419 tbhits 0 time 4646 pv b1c3 b8c6 info depth 34",
+                BoardEvaluationType = EvaluationType.MateInN
+            };
+
+            var expectedAnalysis = new BoardAnalysis
+            {
+                BoardEvaluation = -11,
+                Analysis = "score mate -11 nodes 4884896 nps 1051419 tbhits 0 time 4646 pv Nc3 Nc6 info depth 34",
+                BoardEvaluationType = EvaluationType.MateInN
+            };
+            
+            var newAnalysis = processor.ProcessBoardAnalysis(10, 1, boardAnalysis);
+            Assert.AreEqual(expectedAnalysis, newAnalysis, "Expected analysis text to have move substituted in");
+            mock.VerifyAllExpectations();
+        }
+    }
+}
---
+++ WebEngine/Repositories/AnalysisSimplifier.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using RedChess.ChessCommon;
+using RedChess.ChessCommon.Enumerations;
+using RedChess.ChessCommon.Interfaces;
+using RedChess.EngineFactory;
+using RedChess.WebEngine.Repositories.Interfaces;
+
+namespace RedChess.WebEngine.Repositories
+{
+    internal class AnalysisSimplifier
+    {
+        private readonly IHistoryRepository m_historyRepository;
+
+        public AnalysisSimplifier(IHistoryRepository historyRepository)
+        {
+            m_historyRepository = historyRepository;
+        }
+
+        public IBoardAnalysis ProcessBoardAnalysis(int gameId, int moveNumber, IBoardAnalysis inputAnalysis)
+        {
+            var outputAnalysis = new BoardAnalysis(inputAnalysis);
+            var historyEntry = m_historyRepository.FindByGameIdAndMoveNumber(gameId, moveNumber);
+            using (var board = BoardFactory.CreateInstance())
+            {
+                /* score mate -11 nodes 4884896 nps 1051419 tbhits 0 time 4646 pv 
+                 * c4e2 d7b5 g1e1 b2d2 f2f1 d2f4 f1g2 b5e2 e1e2 f4f3 g2h2 f3e2 h2g3 
+                 * e2f3 g3h2 e4e3 h2g1 e3e2 g1h2 e2e1n h2g1 f3g2 info depth 34 */
+                board.FromFen(historyEntry.Fen);
+                if (outputAnalysis.BoardEvaluationType == EvaluationType.MateInN)
+                {
+                    var rx = new Regex(@"score mate -?\d+ nodes \d+ nps \d+ tbhits \d+ time \d+ pv (.*?) info depth");
+                    var matches = rx.Matches(outputAnalysis.Analysis);
+                    if (matches.Count > 0)
+                    {
+                        var lastMatch = matches[matches.Count - 1];
+                        var matchBeginsAt = lastMatch.Groups[1].Index;
+                        var moves = lastMatch.Groups[1].Value.Split(' ');
+                        foreach (var move in moves)
+                        {
+                            var locations = MovesToLocations(new[] { move.Substring(0, 2), move.Substring(2, 2) });
+                            board.Move(locations[0], locations[1]);
+                            var lastMove = board.LastMove();
+                            outputAnalysis.Analysis = outputAnalysis.Analysis.Remove(matchBeginsAt, move.Length).Insert(matchBeginsAt, lastMove);
+                            matchBeginsAt += lastMove.Length + 1; // include the space
+                        }
+                    }
+                }
+            }
+
+            return outputAnalysis;
+        }
+
+        private IList<Location> MovesToLocations(IEnumerable<string> locations)
+        {
+            return locations.Select(s => (Location)Enum.Parse(typeof(Location), s.ToUpperInvariant())).ToList();
+        }
+
+    }
+}
