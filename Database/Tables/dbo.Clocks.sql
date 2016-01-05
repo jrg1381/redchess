0abb6edf84887f738d39859422983fedb91e710a
James Gilmore <james.gilmore@red-gate.com>
2016-01-05 21:54:16 +0000

    Don't need these columns now

--- Database/Tables/dbo.Clocks.sql
+++ Database/Tables/dbo.Clocks.sql
@@ -7,9 +7,7 @@ CREATE TABLE [dbo].[Clocks]
 [TimeElapsedBlackMs] [int] NOT NULL,
 [TimeElapsedWhiteMs] [int] NOT NULL,
 [TimeLimitMs] [int] NOT NULL,
-[PlayersReady] [int] NOT NULL,
-[WhiteReady] [bit] NOT NULL CONSTRAINT [DF_Clocks_WhiteReady] DEFAULT ((0)),
-[BlackReady] [bit] NOT NULL CONSTRAINT [DF_Clocks_BlackReady] DEFAULT ((0))
+[PlayersReady] [int] NOT NULL
 )
 ALTER TABLE [dbo].[Clocks] ADD
 CONSTRAINT [FK_Cascade_clocks] FOREIGN KEY ([GameId]) REFERENCES [dbo].[Boards] ([GameId]) ON DELETE CASCADE
