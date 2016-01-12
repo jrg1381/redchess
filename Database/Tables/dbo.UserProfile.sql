ef4d59c3b4e09edc23ae410767777d9d9fec4e62
James Gilmore <james.gilmore@red-gate.com>
2016-01-12 13:41:54 +0000

    Add index on metadata. Add EmailHash column

--- Database/Tables/dbo.UserProfile.sql
+++ Database/Tables/dbo.UserProfile.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[UserProfile]
 (
 [UserId] [int] NOT NULL IDENTITY(1, 1),
-[UserName] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[UserName] [nvarchar] (max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[EmailHash] [nvarchar] (32) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 GO
 ALTER TABLE [dbo].[UserProfile] ADD CONSTRAINT [PK_dbo.UserProfile] PRIMARY KEY CLUSTERED  ([UserId])
