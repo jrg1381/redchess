9593cf1ffe135de17ebd74efbc4b4d92eda462a4
James Gilmore <james.gilmore@red-gate.com>
2016-02-04 22:14:46 +0000

    Add test that API call 404s when there is an exception

--- ControllerTests/ChessApiTests.cs
+++ ControllerTests/ChessApiTests.cs
@@ -1,10 +1,15 @@
-﻿using Chess.Controllers;
+﻿using System.Collections.Generic;
+using Chess.Controllers;
 using NUnit.Framework;
 using RedChess.WebEngine.Models;
 using RedChess.WebEngine.Repositories.Interfaces;
 using Rhino.Mocks;
 using System.Dynamic;
+using System.Linq;
+using System.Net;
 using System.Web.Http.Results;
+using System.Web.Mvc;
+using RedChess.ChessCommon.Interfaces;
 using RedChess.WebEngine.Repositories;
 
 namespace RedChess.ControllerTests
@@ -12,26 +17,56 @@ namespace RedChess.ControllerTests
     [TestFixture]
     public class ChessApiTests
     {
-        [Test]
-        public void MovesReturnsMoves()
+        private IEnumerable<TestCaseData> GamesForTestingMovesApi()
+        {
+            yield return new TestCaseData(
+                @"{""Moves"":[{""Fen"":""rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""""},{""Fen"":""rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""e4""}],""Description"":""clive vs james"",""IsParticipant"":true,""Analysis"":null,""GameOver"":false,""Status"":null,""Winner"":""""}",
+                new FakeGame().Build()
+                );
+
+            yield return new TestCaseData(
+                @"{""Moves"":[{""Fen"":""rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""""},{""Fen"":""rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""e4""}],""Description"":""clive vs james"",""IsParticipant"":true,""Analysis"":null,""GameOver"":true,""Status"":null,""Winner"":""""}",
+                new FakeGame().GameOver().Build()
+                );
+            yield return new TestCaseData(
+                @"{""Moves"":[{""Fen"":""rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""""},{""Fen"":""rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""e4""}],""Description"":""clive vs james"",""IsParticipant"":true,""Analysis"":null,""GameOver"":true,""Status"":null,""Winner"":""w""}",
+                new FakeGame().WhiteWins().Build()
+                );
+            yield return new TestCaseData(
+                @"{""Moves"":[{""Fen"":""rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""""},{""Fen"":""rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""e4""}],""Description"":""clive vs james"",""IsParticipant"":true,""Analysis"":null,""GameOver"":true,""Status"":null,""Winner"":""b""}",
+                new FakeGame().BlackWins().Build()
+                );
+        }
+
+        [TestCaseSource(nameof(GamesForTestingMovesApi))]
+        public void MovesReturnsMoves(string expectedJson, GameDto gameDto)
         {
             var historyEntries = GetHistoryEntries();
 
-            var expectedJson =
-                @"{""Moves"":[{""Fen"":""rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""""},{""Fen"":""rnbqkbnr/pppp1ppp/8/4p3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0"",""Move"":""e4""}],""Description"":""clive vs james"",""IsParticipant"":true,""Analysis"":null,""GameOver"":false,""Status"":null,""Winner"":""""}";
-
-            var fakeGame = new FakeGame().Build();
             var mockManager = MockRepository.GenerateMock<IGameManager>();
-            var gameBinding = new GameBinding(fakeGame, mockManager);
+            var gameBinding = new GameBinding(gameDto, mockManager); 
 
-            mockManager.Expect(x => x.FetchGame(FakeGame.DefaultGameId)).Return(gameBinding);
-            mockManager.Expect(x => x.FindAllMoves(Arg<int>.Is.Equal(FakeGame.DefaultGameId))).Return(historyEntries);
-            var controller = new ChessApiController(mockManager, FakeGame.StubIdentityProviderFor(fakeGame.UserProfileWhite.UserName, fakeGame.UserProfileWhite.UserId));
-            var result = controller.Moves(FakeGame.DefaultGameId) as JsonResult<ExpandoObject>;
+            mockManager.Expect(x => x.FetchGame(gameDto.GameId)).Return(gameBinding);
+            mockManager.Expect(x => x.FindAllMoves(Arg<int>.Is.Equal(gameDto.GameId))).Return(historyEntries);
+            var controller = new ChessApiController(mockManager, FakeGame.StubIdentityProviderFor(gameDto.UserProfileWhite.UserName, gameDto.UserProfileWhite.UserId));
+            var result = controller.Moves(gameDto.GameId) as JsonResult<ExpandoObject>;
             dynamic d = result.Content;
             mockManager.VerifyAllExpectations();
             var json = Newtonsoft.Json.JsonConvert.SerializeObject(d);
             Assert.AreEqual(expectedJson, json, "expected JSON for getting moves not correct");
+        }
+
+        [Test]
+        public void MovesReturns404WhenNoGameOrNoMoves()
+        {
+            var mockManager = MockRepository.GenerateMock<IGameManager>();
+
+            mockManager.Expect(x => x.FetchGame(Arg<int>.Is.Anything)).Return(null);
+            mockManager.Expect(x => x.FindAllMoves(Arg<int>.Is.Anything)).Return(Enumerable.Empty<HistoryEntry>());
+            var controller = new ChessApiController(mockManager, FakeGame.StubIdentityProviderFor("james"));
+            var result = controller.Moves(1234) as StatusCodeResult;
+            mockManager.VerifyAllExpectations();
+            Assert.AreEqual(HttpStatusCode.NotFound, result.StatusCode, "Expected 404 when game does not exist");
         }
 
         private static HistoryEntry[] GetHistoryEntries()
