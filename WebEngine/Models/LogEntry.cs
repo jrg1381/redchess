a593d500f744f581a6fb59b0381df9082c53dbc2
James Gilmore <james.gilmore@red-gate.com>
2016-02-06 22:45:29 +0000

    Fix the log formatting

--- WebEngine/Models/LogEntry.cs
+++ WebEngine/Models/LogEntry.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
@@ -37,13 +38,33 @@ namespace RedChess.WebEngine.Models
         public DateTimeOffset Timestamp { get; set; }
         public string ETag { get; set; }
 
+#if DEBUG
+        private static string LoremIpsum(int length)
+        {
+            var loremIpsum =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ";
+
+            if (loremIpsum.Length < length)
+            {
+                var sb = new StringBuilder(loremIpsum);
+                while (sb.Length < length)
+                {
+                    sb.Append(loremIpsum);
+                }
+                loremIpsum = sb.ToString();
+            }
+
+            return loremIpsum.Substring(0, length);
+        }
+#endif
+
         public static IEnumerable<LogEntry> FetchLogEntries(int entryCount)
         {
 #if DEBUG
             return new List<LogEntry>
             {
-                new LogEntry() {Message = "Helloworld", PreciseTimeStamp = DateTime.Now},
-                new LogEntry() {Message = "Helloworld2", PreciseTimeStamp = DateTime.Now.AddSeconds(33)}
+                new LogEntry() {Message = LoremIpsum(1024), PreciseTimeStamp = DateTime.Now},
+                new LogEntry() {Message = LoremIpsum(512), PreciseTimeStamp = DateTime.Now.AddSeconds(33)}
             };
 #endif
 
