bb68de0aa1b6fda5d841461ea66ccf3a2ac4b74f
James Gilmore <james.gilmore@red-gate.com>
2016-02-17 22:25:41 +0000

    Simple caching layer

--- Chess/Controllers/AccessValidator.cs
+++ Chess/Controllers/AccessValidator.cs
@@ -1,4 +1,6 @@
-﻿using RedChess.WebEngine.Repositories.Interfaces;
+﻿using System;
+using RedChess.ChessCommon;
+using RedChess.WebEngine.Repositories.Interfaces;
 
 namespace Chess.Controllers
 {
@@ -6,6 +8,12 @@ namespace Chess.Controllers
     {
         private readonly IGameManager m_GameManager;
         private readonly ICurrentUser m_IdentityProvider;
+        private static readonly MruCache<Tuple<int, int>, bool> m_Cache;
+
+        static AccessValidator()
+        {
+            m_Cache = new MruCache<Tuple<int, int>, bool>(25);
+        }
 
         public AccessValidator(IGameManager gameManager, ICurrentUser identityProvider)
         {
@@ -15,10 +23,23 @@ namespace Chess.Controllers
 
         public bool MayAccess(int gameId)
         {
-            var game = m_GameManager.FetchGame(gameId);
             var userId = m_IdentityProvider.CurrentUserId;
+            bool allowed;
+            var gameIdUserIdTuple = new Tuple<int, int>(gameId, userId);
 
-            return (game.UserProfileBlack.UserId == userId || game.UserProfileWhite.UserId == userId);
+            if (m_Cache.Get(gameIdUserIdTuple, out allowed))
+            {
+                return allowed;
+            }
+            else
+            {
+                var game = m_GameManager.FetchGame(gameId);
+                if (game == null)
+                    return false;
+                allowed = (game.UserProfileBlack.UserId == userId || game.UserProfileWhite.UserId == userId);
+                m_Cache.Set(gameIdUserIdTuple, allowed);
+                return allowed;
+            }
         }
     }
 }
---
+++ ChessCommon/MruCache.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace RedChess.ChessCommon
+{
+    public class MruCache<TK,TV>
+    {
+        private readonly Dictionary<TK, TV> m_data;
+        private readonly TK[] m_orderedKeys;
+        private int m_index = 0;
+        private readonly int m_size;
+
+        public MruCache(int size)
+        {
+            m_data = new Dictionary<TK, TV>(size);
+            m_orderedKeys = new TK[size];
+            m_size = size;
+        }
+
+        public void Set(TK key, TV value)
+        {
+            m_data[key] = value;
+            if (m_orderedKeys[m_index] != null)
+                m_data.Remove(key);
+            m_orderedKeys[m_index] = key;
+            m_index = (m_index + 1) % m_size;
+        }
+
+        public bool Get(TK key, out TV value)
+        {
+            return m_data.TryGetValue(key, out value);
+        }
+    }
+}
