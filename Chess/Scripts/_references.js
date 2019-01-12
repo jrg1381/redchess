079c8654a52d4e91ed945f1bd1623d7fcd9047b6
James Gilmore <jamesg@speechmatics.com>
2019-01-12 14:47:45 +0000

    Fix some signalR references

--- Chess/Scripts/_references.js
+++ Chess/Scripts/_references.js
@@ -1,6 +1,6 @@
 ﻿/// <reference path="jquery-ui-1.12.1.js" />
 /// <reference path="jquery-3.3.1.js" />
-/// <reference path="~/Scripts/jquery.signalR-2.2.1.js" />
+/// <reference path="~/Scripts/jquery.signalR-2.4.0.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
 
